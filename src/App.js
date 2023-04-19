8b386dedf7af11070ceeb2f1e6f43f05acaf36e1
sarathchandt <sarathchand770@gmail.com>
2023-04-19 18:57:06 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState } from 'react';
+
+
+
 
 function App() {
+  const [toDos,setTodos] = useState([]);
+  const [toDo,setTodo] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="app">
+      <div className="mainHeading">
+        <h1>ToDo List</h1>
+      </div>
+      <div className="subHeading">
+        <br />
+        <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
+      </div>
+      <div className="input">
+        <input onChange={(e)=>{
+          setTodo(e.target.value)
+        }} value={toDo} type="text" placeholder="🖊️ Add item..." />
+        <i  onClick={()=>{
+          setTodos([...toDos,{id : Date.now(), value : toDo, status : false}])
+            }
+          } className="fas fa-plus"></i>
+      </div>
+
+
+      <div className="todos">
+       {
+       toDos.map((obj)=>{
+      return  (
+        <div className="todo">
+        <div className="left">
+          <input onChange={(e)=>{
+            setTodos(toDos.filter(obj2 => { 
+              if(obj.id===obj2.id){
+                obj2.status=e.target.checked;
+                
+              }
+              
+              return obj2;
+             }))
+          }} 
+            value={obj.status}  type="checkbox" name="" id="" />
+          <p>{obj.value}</p>
+        </div>
+        <div className="right">
+          <i onClick={()=>{
+            setTodos(toDos.filter(obj3=>{
+              
+              if(obj3.id !== obj.id){
+               return obj3;
+              }
+              return null;
+            }))
+          }} className="fas fa-times"></i>
+        </div>
+      </div>
+        )
+       })
+   
+}
+      </div>
+      <br />
+      <h1> LIST THAT YOU HAVE DONE...! </h1>
+
+      {
+        toDos.map((obj)=>{
+          console.log(obj);
+          if(obj.status){
+            return( <h1>* {obj.value}</h1> )
+          }
+          return null;
+        })
+      }
     </div>
-  );
+    </div>
+  ); 
 }
 
 export default App;
